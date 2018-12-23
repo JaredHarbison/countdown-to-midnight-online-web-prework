9e7c8ff92c5de969ce68104f261e74e31ef0afca
Jared Harbison <jared.harbison@gmail.com>
2018-12-23 18:55:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,10 +3,10 @@ def countdown (number)
   while counter < number
   puts "#{number} SECOND(S)!"
   number -= 1
+  end
     if number == 0 
     puts "HAPPY NEW YEAR!"
   end
-end
 end
 
 def countdown_with_sleep (number)
