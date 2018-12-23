eba6e1c022f71b6b91e7ba7d82c3c493c0cbf7f4
Jared Harbison <jared.harbison@gmail.com>
2018-12-23 18:54:56 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
-#write your code here
+def countdown (number)
+  counter = 0
+  while counter < number
+  puts "#{number} SECOND(S)!"
+  number -= 1
+    if number == 0 
+    puts "HAPPY NEW YEAR!"
+  end
+end
+end
 
-def countdown
+def countdown_with_sleep (number)
+    counter = 0
+  while counter < number
+  puts "#{number} SECOND(S)!"
+  number -= 1
+  sleep (1)
+  end
 end
